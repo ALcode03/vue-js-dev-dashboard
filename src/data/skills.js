beabb966e9aaf1f5216d375756c99b1a4b49ce7b
Alexandra KEDJU <alexandrakedju@gmail.com>
2026-06-25 15:29:14 +0200

    Skills page created

---
+++ src/data/skills.js
@@ -0,0 +1,76 @@
+export const skills = [
+           {
+            id: 1,
+            name: "Vue.js",
+            category: "frontend",
+            level: "Advanced"
+           },
+
+            {
+            id: 2,
+            name: "Node.js",
+            category: "backend",
+            level: "Advanced"
+           },
+
+            {
+            id: 3,
+            name: "React.js",
+            category: "frontend",
+            level: "Expert"
+           },
+
+            {
+            id: 4,
+            name: "Nest.js",
+            category: "backend",
+            level: "Advanced"
+           },
+
+           {
+            id: 5,
+            name: " PostgrSQL",
+            category: "database",
+            level: "beginner"
+           },
+
+             {
+            id: 6,
+            name: "Bootstrap",
+            category: "frontend",
+            level: "intermediate"
+           },
+
+
+             {
+            id: 7,
+            name: "JavaScript",
+            category: "frontend",
+            level: "expert"
+           },
+
+
+             {
+            id: 8,
+            name: "Docker",
+            category: "devops",
+            level: "beginner"
+           },
+
+
+            {
+            id: 9,
+            name: "TypeScript",
+            category: "frontend",
+            level: "beginner"
+           },
+
+
+             {
+            id: 10,
+            name: "Git&Github",
+            category: "backend",
+            level: "Advanced"
+           }
+
+];
