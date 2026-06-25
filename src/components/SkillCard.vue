187a248200b6bed16f420e300e23e42ac6ae4a93
Alexandra KEDJU <alexandrakedju@gmail.com>
2026-06-25 15:28:42 +0200

    skillCard created

---
+++ src/components/SkillCard.vue
@@ -0,0 +1,22 @@
+<script setup>
+defineProps(
+    {
+        skill: Object
+    }
+);
+
+</script>
+
+<template>
+
+     <div class="skill-card">
+
+        <h2>{{ skill.name }}</h2>
+
+        <h3>{{ skill.category }}</h3>
+
+        <p>{{ skill.level }}</p>
+
+     </div>
+
+</template>
